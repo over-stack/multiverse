5e135e18d3d9df13a873ce408e07e322b80b85c8
Michael <gibiktus@mail.ru>
2019-08-13 11:50:51 +0300

    Big refactoring 2. (New files)

---
+++ com.gbk.multiverse/my_libs.py
@@ -0,0 +1,60 @@
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def __repr__(self):
+        return f'X: {self.x}, Y: {self.y}'
+
+    def get_tuple(self):
+        return self.x, self.y
+
+
+class Rect:
+    def __init__(self, x, y, width, height, isCenter=False):
+        self.width = width
+        self.height = height
+        self.move_to(x, y, isCenter)
+
+    def __repr__(self):
+        return f'Left: {self.left}, Top: {self.top}, Width: {self.width}, Height: {self.height}, Center: {self.center}'
+
+    def intersects(self, rect):
+        intersects = True
+        if self.top > rect.bottom or self.bottom < rect.top:
+            intersects = False
+        elif self.right < rect.left or self.left > rect.right:
+            intersects = False
+
+        return intersects
+
+    def move(self, x, y):
+        self.left += x
+        self.right += x
+        self.center.x += x
+        self.topleft.x += x
+
+        self.top += y
+        self.bottom += y
+        self.center.y += y
+        self.topleft.y += y
+
+    def move_to(self, x, y, isCenter=False):
+        if isCenter:
+            self.center = Vector2D(x, y)
+            self.left = self.center.x - self.width / 2
+            self.top = self.center.y - self.height / 2
+        else:
+            self.center = Vector2D(x + self.width / 2, y + self.height / 2)
+            self.left = x
+            self.top = y
+
+        self.right = self.left + self.width
+        self.bottom = self.top + self.height
+        self.topleft = Vector2D(self.left, self.top)
+
+    def get_tuple(self):
+        return self.left, self.top, self.width, self.height
---
+++ com.gbk.multiverse/sun.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Sun:
+    def __init__(self, cam_width, cam_height):
+        self.img = pygame.Surface((cam_width, cam_height))
+        self.img.fill((0, 0, 0))
+        self.brightness = 255
+
+    def update(self, time):
+        self.brightness -= 0.1 * time
+        self.img.set_alpha(255 - self.brightness)
